845cf8d2c265b3a201628f42163078839bf0e305
Andrew Kuchling <amk@amk.ca>
2010-12-08 21:57:36 -0500

    Regenerate links, .htaccess

--- sherlock-holmes/links.h
+++ sherlock-holmes/links.h
@@ -16,12 +16,11 @@
 <H3>Other&nbsp;Collections</H3>
 <LI><A HREF="../">Index</a>
 <LI><A HREF="../comics/">Comics</A>
-<LI><A HREF="../cryptography/">Cryptography</A>
 <LI><A HREF="../quotations/">Commonplace&nbsp;book</A>
 <LI><A HREF="../doctor-who/">Doctor&nbsp;Who</A>
 <LI><A HREF="../neil-gaiman/">Neil&nbsp;Gaiman</A>
 <LI><A HREF="../peter-greenaway/">Peter&nbsp;Greenaway</A>
+<LI><A HREF="../hp-lovecraft/">H.P.&nbsp;Lovecraft</A>
 <LI><A HREF="../python-quotes/">Python</A>
-<LI><A HREF="../robertson-davies/">Robertson&nbsp;Davies</A>
+<LI><A HREF="../shakespeare/">Shakespeare</A>
 <LI><A HREF="../sherlock-holmes/">Sherlock&nbsp;Holmes</A>
-<LI><A HREF="../tom-baker/">Tom&nbsp;Baker</A>
